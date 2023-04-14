219e79dfc5d13618a1f51b56dc3230bf9f2507c0
cakhero <129480707+cakhero@users.noreply.github.com>
2023-04-15 01:49:34 +0700

    Add files via upload

---
+++ pi2.c
@@ -0,0 +1,43 @@
+// a simple parallel pi program version 2 (1st SPMD padded), pi program padded accumulator
+
+#include <omp.h>
+#include <stdio.h>
+
+static long num_steps = 1000000000;
+double step;
+
+#define PAD 8
+#define NUM_THREADS 2
+
+void main() {
+
+	int i, nthreads;
+	double pi, tdata, sum[NUM_THREADS][PAD];
+
+	tdata = omp_get_wtime();
+
+	step = 1.0 / (double) num_steps;
+	omp_set_num_threads(NUM_THREADS);
+
+	#pragma omp parallel
+	{
+		int i, id, nthrds;
+		double x;
+
+		id = omp_get_thread_num();
+		nthrds = omp_get_num_threads();
+		
+		if (id == 0) nthreads = nthrds;
+
+		for (i = id, sum[id][0] = 0.0; i < num_steps; i = i + nthrds) {
+			x = (i + 0.5) * step;
+			sum[id][0] += 4.0 / (1.0 + x * x);
+		}
+	}
+	
+	for (i = 0, pi = 0.0; i < nthreads; i++) pi += sum[i][0] * step;
+
+	tdata = omp_get_wtime() - tdata;
+	
+	printf("pi = %f in %f seconds\n", pi, tdata);
+}
